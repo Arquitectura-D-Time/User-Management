edc11baea975c43af35063bde86675d41da3635f
David Fernando <dafguerreroal@unal.edu.co>
2019-12-18 19:02:06 -0500

    error mrk por Fernando
    Perdon

--- common/driver.go
+++ common/driver.go
@@ -18,13 +18,14 @@ var dbConn = &DB{}
 
 // ConnectSQL ...
 func ConnectSQL(host, port, uname, pass, dbname string) (*DB, error) {
-	dbSource := fmt.Sprintf(
+	/*dbSource := fmt.Sprintf(
 		"root:%s@tcp(%s:%s)/%s?charset=utf8",
 		pass,
 		host,
 		port,
 		dbname,
 	)
+	*/
 	d, err := sql.Open("mysql", "root:123@tcp(http://user-management-db:3005)/userManagement")
 	if err != nil {
 		panic(err)
